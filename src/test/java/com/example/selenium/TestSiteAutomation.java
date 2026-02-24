712ed307d206ccca011326955b734301575cfca4
Prathamesh Patil <patilprathamesh76@gmail.com>
2026-02-24 17:06:26 +0100

    added headless

--- src/test/java/com/example/selenium/TestSiteAutomation.java
+++ src/test/java/com/example/selenium/TestSiteAutomation.java
@@ -26,7 +26,7 @@ public class TestSiteAutomation {
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
